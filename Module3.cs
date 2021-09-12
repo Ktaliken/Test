871f3be64d17c771b6d647d0c72de23ed99ac6dd
Ktaliken <89549633+Ktaliken@users.noreply.github.com>
2021-09-12 16:06:03 +0300

    Add files via upload

---
+++ Module3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace module_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Nick";
+            byte Age = 26;
+            bool Pet = true;
+            byte ShoeSize = 41;
+
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("I am " + Age + " years old");
+            Console.WriteLine("Do i have a pet? " + Pet);
+            Console.WriteLine("My shoe size is " + ShoeSize);
+        }
+    }
+}
