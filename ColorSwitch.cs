1fac0397d9a3103c8d1bff331f17fee742a97957
Ktaliken <gubarev.n.i@gmail.com>
2021-09-18 18:40:23 +0300

    Добавлен файл программы ColorSwitch

---
+++ ColorSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class MainClass
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        for (int i = 1; i < 5; i++)
+        {
+            Console.WriteLine("Iteration {0}", i);
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is Red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is Green!");
+                    break;
+
+                case "blue":
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Youre color is Blue!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is Yellow!");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
