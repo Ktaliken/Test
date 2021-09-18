838f33448f4aa797f1c127fd682733d6cd44d274
Ktaliken <gubarev.n.i@gmail.com>
2021-09-18 19:54:11 +0300

    Добавлен файл NameChange.cs

---
+++ NameChange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите своё имя");
+
+        var name = Console.ReadLine();
+
+        Console.WriteLine("Ваше имя в обратном порядке");
+
+        for (int i = name.Length - 1; i >= 0; i--)
+        {
+            Console.Write(name[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
