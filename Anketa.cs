6675dfb70104ce87c4b226b0eb480c0b2e600553
Ktaliken <gubarev.n.i@gmail.com>
2021-09-19 19:44:12 +0300

    Добавлен файл Anketa.cs

---
+++ Anketa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		(string name, int age) anketa;
+
+		Console.WriteLine("Введите ваше имя: ");
+		anketa.name = Console.ReadLine();
+
+		Console.WriteLine("Введите ваш возраст: ");
+		anketa.age = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Вас зовут {0}, и вам {1} лет", anketa.name, anketa.age);
+
+		Console.ReadKey();
+	}
+}
