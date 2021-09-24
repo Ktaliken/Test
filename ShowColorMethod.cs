adbd3fc3ed06221c6d7f47bbb405360a87fc330d
Ktaliken <gubarev.n.i@gmail.com>
2021-09-24 21:09:55 +0300

    Добавлен файл ShowColorMethod.cs

---
+++ ShowColorMethod.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var favcolors = new string[3];
+
+
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+
+            Console.WriteLine("Ваши любимые цвета:");
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
+            Console.ReadKey();
+        }
+
+
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "yellow":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+                    Console.WriteLine("Your color is out of list");
+                    break;
+            }
+            return color;
+
+
+        }
+
+
+
+    }
+}
